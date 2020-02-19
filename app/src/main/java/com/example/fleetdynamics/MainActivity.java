33665a798debb879c20c9a0e4433967cdb762c4a
M-Wassell <55836302+M-Wassell@users.noreply.github.com>
2020-02-19 14:20:36 +0000

    Firebase used for API/ database.

--- app/src/main/java/com/example/fleetdynamics/MainActivity.java
+++ app/src/main/java/com/example/fleetdynamics/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fleetdynamics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         // Jeni work
 
 
