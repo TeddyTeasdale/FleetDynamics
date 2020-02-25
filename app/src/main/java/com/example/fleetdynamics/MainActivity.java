dfe5b89edfbeb5cd3aaee023dabfaa57af75937a
t7091808 <teddyteasdale@gmail.com>
2020-02-25 16:31:42 +0000

    Added Job schedule screen and began implmenting the job list the navigation screen is set up to link to each screen pending merge(access to other screens)

--- app/src/main/java/com/example/fleetdynamics/MainActivity.java
+++ app/src/main/java/com/example/fleetdynamics/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.fleetdynamics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
 
         // Jeni work
@@ -46,5 +50,11 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Teddy work
+         Button button= (Button) findViewById(R.id.LogInButton);
+               button.setOnClickListener(new View.OnClickListener() {
+                   public void onClick(View v) {
+                     startActivity(new Intent(MainActivity.this, NavigationActivity.class));
+                    }
+                });
     }
 }
