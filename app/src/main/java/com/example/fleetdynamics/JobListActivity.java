749cfd25c95bb763307d9da4797555fa2b49a474
t7091808 <teddyteasdale@gmail.com>
2020-02-27 13:31:02 +0000

    Can now retrieve database entries and display them

--- app/src/main/java/com/example/fleetdynamics/JobListActivity.java
+++ app/src/main/java/com/example/fleetdynamics/JobListActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class JobListActivity extends AppCompatActivity
 {
@@ -21,9 +24,27 @@ public class JobListActivity extends AppCompatActivity
 
     public void InitJobList()
     {
+        // .child can be used to seperate the pathing out more logically
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("Bhz7iRP0Pd5V3syUDbVV");
+        final DatabaseReference myRef = database.getReference("Job");
 
-        System.out.println(myRef.toString());
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String address1 = dataSnapshot.child("1").child("Address").getValue(String.class);
+
+                System.out.println(address1);
+                System.out.println(myRef.getKey());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
     }
 }
