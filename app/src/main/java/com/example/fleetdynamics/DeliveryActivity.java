9be152f816398ba326dae858ec4ccc6cae2a39a1
Hafsa Farhat <hafsa@gmx.co.uk>
2020-03-03 14:13:31 +0000

    signature buttons

--- app/src/main/java/com/example/fleetdynamics/DeliveryActivity.java
+++ app/src/main/java/com/example/fleetdynamics/DeliveryActivity.java
@@ -4,31 +4,108 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import com.kyanogen.signatureview.SignatureView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
 
 public class DeliveryActivity extends AppCompatActivity {
-    static final int REQUEST_IMAGE_CAPTURE = 1;
 
+    Button cancel,clear, save;
+    Bitmap bit;
+    SignatureView SignatureView;
+    String view;
+    Dialog dis;
+    private static final String IMAGE_DIRECTORY = "/signdemo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery);
 
+        SignatureView = (SignatureView) findViewById(R.id.delsignature);
+        clear = (Button) findViewById(R.id.delclear);
+        save = (Button) findViewById(R.id.delieverysave);
+        cancel = (Button) findViewById(R.id.deliverycancel);
 
-    }
-    private void ClickPhoto()
-    {
-        Intent takePicture = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePicture.resolveActivity(getPackageManager()) != null)
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SignatureView.clearCanvas();
+
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("tag", "cancelled");
+                dis.dismiss();
+                recreate();
+
+            }
+        });
+
+
+
+        save.setOnClickListener(new View.OnClickListener()
         {
-            startActivityForResult(takePicture, REQUEST_IMAGE_CAPTURE);
-        }
+            @Override
+            public void onClick(View v)
+            {
+                bit = SignatureView.getSignatureBitmap();
+                view = saveImage (bit);
+            }
+        });
+    }
 
+    private String saveImage(Bitmap mybit)
+    {
+        ByteArrayOutputStream delivery = new ByteArrayOutputStream();
+        mybit.compress(Bitmap.CompressFormat.JPEG, 90, delivery);
+        File pic = new File(
+                Environment.getExternalStorageState() + IMAGE_DIRECTORY);
+                if (!pic.exists()) {
+                    pic.mkdirs();
+                    Log.d("hello", pic.toString());
+                }
+
+                try
+                {
+                    File pc = new File(pic, Calendar.getInstance().getTimeInMillis() + "jpg");
+                    pc.createNewFile();
+                    FileOutputStream pi = new FileOutputStream(pc);
+                    pi.write(delivery.toByteArray());
+                    MediaScannerConnection.scanFile(DeliveryActivity.this,
+                                new String[] {pc.getPath()},
+                                new String[] {"image/jpeg"}, null);
+                    pi.close();
+                    Log.d("TAG", "File is saved" + pc.getAbsolutePath());
+                    return pc.getAbsolutePath();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return "";
 
     }
 }
+
+
+
+
+
 
