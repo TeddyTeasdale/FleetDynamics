0eaa9801eb21edc1a12a64103a0da3ce09eb6dac
t7091808 <rsresdf>
2020-03-04 16:43:30 +0000

    Implemented Nav draw and nav draw toggle button

--- app/src/main/java/com/example/fleetdynamics/MainActivity.java
+++ app/src/main/java/com/example/fleetdynamics/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.fleetdynamics;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
@@ -32,10 +33,25 @@ public class MainActivity extends AppCompatActivity
 
         drawer = findViewById(R.id.drawer_layout);
 
-        ActionBarDrawerToggle
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer ,toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
 
 
     }
 
+    @Override
+    public void onBackPressed()
+    {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
 }
