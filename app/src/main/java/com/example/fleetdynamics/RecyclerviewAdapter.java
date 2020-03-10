bda5afcdaf83b169e338bb01362d3f81a6651a5c
t7091808 <sdas>
2020-03-10 13:45:57 +0000

    Merge branch 'Dev' into Micheal-Wassell
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	.idea/caches/gradle_models.ser
    #	.idea/libraries/Gradle__com_google_android_gms_play_services_basement_17_0_0_aar.xml
    #	.idea/libraries/Gradle__com_google_firebase_firebase_common_19_3_0_aar.xml
    #	.idea/libraries/Gradle__com_google_firebase_firebase_components_16_0_0_aar.xml
    #	.idea/workspace.xml
    #	app/app.iml
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/fleetdynamics/MainActivity.java
    #	local.properties

--- app/src/main/java/com/example/fleetdynamics/RecyclerviewAdapter.java
+++ app/src/main/java/com/example/fleetdynamics/RecyclerviewAdapter.java
@@ -9,9 +9,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+// Author Teddy Teasdale
+
 public class RecyclerviewAdapter extends RecyclerView.Adapter<RecyclerviewAdapter.MyViewHolder>
 {
     private ArrayList<Job> jobList;
+
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
         // each data item is just a string in this case
@@ -50,17 +53,10 @@ public class RecyclerviewAdapter extends RecyclerView.Adapter<RecyclerviewAdapte
     public void onBindViewHolder(MyViewHolder holder, int position)
     {
         Job job = jobList.get(position);
-
-        String customer =  job.getCustomerName();
-        String vehicle = job.getVehicle();
-        String address = job.getJobLocation();
-        String jobType = job.getType();
-
-
-        holder.customerTextView.setText(customer);
-        holder.addressTextView.setText(address);
-        holder.jobTypeTextView.setText(jobType);
-        holder.vehicleTextView.setText(vehicle);
+        holder.customerTextView.setText(job.getCustomerName());
+        holder.addressTextView.setText(job.getJobLocation());
+        holder.jobTypeTextView.setText(job.getType());
+        holder.vehicleTextView.setText(job.getVehicle().getModel());
 
 
 
