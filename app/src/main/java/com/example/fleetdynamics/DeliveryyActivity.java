67d848f27040cbbd7f10c0bdc1d594ce98cf0326
t7091808 <sdas>
2020-03-10 16:08:23 +0000

    Merge fixes

--- app/src/main/java/com/example/fleetdynamics/DeliveryyActivity.java
+++ app/src/main/java/com/example/fleetdynamics/DeliveryyActivity.java
@@ -1,3 +1,4 @@
+
 package com.example.fleetdynamics;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,10 @@ import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.kyanogen.signatureview.SignatureView;
 
 import java.io.ByteArrayOutputStream;
@@ -20,12 +25,14 @@ import java.util.Calendar;
 
 public class DeliveryyActivity extends AppCompatActivity {
 
-    Button cancel,clear, save;
+    Button cancel, clear, save;
     Bitmap bit;
     SignatureView SignatureView;
     String view;
     Dialog dis;
     private static final String IMAGE_DIRECTORY = "/signdemo";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,7 @@ public class DeliveryyActivity extends AppCompatActivity {
         save = (Button) findViewById(R.id.delieverysave);
         cancel = (Button) findViewById(R.id.deliverycancel);
 
+
         clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,7 +53,6 @@ public class DeliveryyActivity extends AppCompatActivity {
             }
         });
 
-        // the app is being stopped when clicking cancel button
         //cancel button will cancel the signature action
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,52 +65,48 @@ public class DeliveryyActivity extends AppCompatActivity {
         });
 
 
-       //this button will save the signature as image
-        save.setOnClickListener(new View.OnClickListener()
-        {
+        //this button will save the signature as image
+        save.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 bit = SignatureView.getSignatureBitmap();
-                view = saveImage (bit);
+                view = saveImage(bit);
             }
         });
     }
 
-    private String saveImage(Bitmap mybit)
-    {
+
+
+
+
+    private String saveImage(Bitmap mybit) {
         ByteArrayOutputStream delivery = new ByteArrayOutputStream();
         mybit.compress(Bitmap.CompressFormat.JPEG, 90, delivery);
         File pic = new File(
                 Environment.getExternalStorageState() + IMAGE_DIRECTORY);
-                if (!pic.exists()) {
-                    pic.mkdirs();
-                    Log.d("hello", pic.toString());
-                }
+        if (!pic.exists()) {
+            pic.mkdirs();
+            Log.d("hello", pic.toString());
+        }
 
-                try
-                {
-                    File pc = new File(pic, Calendar.getInstance().getTimeInMillis() + "jpg");
-                    pc.createNewFile();
-                    FileOutputStream pi = new FileOutputStream(pc);
-                    pi.write(delivery.toByteArray());
-                    MediaScannerConnection.scanFile(DeliveryyActivity.this,
-                                new String[] {pc.getPath()},
-                                new String[] {"image/jpeg"}, null);
-                    pi.close();
-                    Log.d("TAG", "File is saved" + pc.getAbsolutePath());
-                    return pc.getAbsolutePath();
+        try {
+            File pc = new File(pic, Calendar.getInstance().getTimeInMillis() + "jpg");
+            pc.createNewFile();
+            FileOutputStream pi = new FileOutputStream(pc);
+            pi.write(delivery.toByteArray());
+            MediaScannerConnection.scanFile(DeliveryyActivity.this,
+                    new String[]{pc.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            pi.close();
+            Log.d("TAG", "File is saved" + pc.getAbsolutePath());
+            return pc.getAbsolutePath();
 
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return "";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
 
     }
+
+
 }
-
-
-
-
-
-
