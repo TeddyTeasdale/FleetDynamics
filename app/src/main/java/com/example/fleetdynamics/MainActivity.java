5f1e35c893f48a3084ed2f3d19c01a0312fb96e1
TeddyTeasdale <55746072+TeddyTeasdale@users.noreply.github.com>
2020-05-05 13:39:13 +0100

    Merge fixes for final build

--- app/src/main/java/com/example/fleetdynamics/MainActivity.java
+++ app/src/main/java/com/example/fleetdynamics/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
